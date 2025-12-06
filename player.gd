c6a880f69523ac8a8340131b4afb68db6be6b92a
Matt V <matthew.vonsanden@gmail.com>
2025-12-06 15:55:18 +1100

    First commit. Basic raytracing integrated into godot scene.

---
+++ player.gd
@@ -0,0 +1,33 @@
+extends CharacterBody3D
+
+var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+var speed = 8.0  # movement speed
+var jump_speed = 6.0  # determines jump height
+var mouse_sensitivity = 0.005  # turning speed
+@onready var cam_parent = $cam_parent
+
+func _init() -> void:
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+
+func get_input():
+	var input = -Input.get_vector("move_left", "move_right", "move_forward", "move_backward")
+
+	var movement_dir = cam_parent.transform.basis * Vector3(input.x, 0, input.y)
+	velocity.x = movement_dir.x * speed
+	velocity.z = movement_dir.z * speed
+
+func _unhandled_input(event):
+	if event is InputEventMouseMotion:
+		cam_parent.rotate_y(-event.relative.x * mouse_sensitivity)
+		cam_parent.rotation.x += event.relative.y * mouse_sensitivity
+		cam_parent.rotation.x = clamp(cam_parent.rotation.x, -PI/2, PI/2) 
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = jump_speed
+	if Input.is_action_just_pressed("escape"):
+		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+
+func _physics_process(delta):
+	velocity.y += -gravity * delta
+	get_input()
+	move_and_slide()
+	print(self.position.y)
---
+++ post_processing.gd
@@ -0,0 +1,5 @@
+extends MeshInstance3D
+
+#Optimisation for the shader (small) by representing it as a triangle
+func _ready():
+	pass
