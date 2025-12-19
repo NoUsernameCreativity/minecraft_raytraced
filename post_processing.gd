ae0939d01b1b0f862caaf938815c6804940a0439
Matt V <matthew.vonsanden@gmail.com>
2025-12-19 16:34:14 +1100

    fixed shader parameters. Can now edit the images. Next is to work on a chunking system.

--- post_processing.gd
+++ post_processing.gd
@@ -1,8 +1,10 @@
 extends MeshInstance3D
 
 #Optimisation for the shader (small) by representing it as a triangle
-@export var cubeDimensions : Vector3 = Vector3(16, 16, 16)
+@export var cubeDimensions : Vector3 = Vector3(32, 64, 32)
 @export var directionalLight : DirectionalLight3D
+
+var finalImageTexture : ImageTexture3D
 
 func _ready():
 	randomize()
@@ -35,13 +37,26 @@ func _ready():
 		imageSlices.append(image)
 	
 	
-	var finalImageTexture = ImageTexture3D.new()
+	finalImageTexture = ImageTexture3D.new()
 	finalImageTexture.create(Image.FORMAT_RGBA8, cubeDimensions.x, cubeDimensions.y, cubeDimensions.z, false, imageSlices)
 	
 	self.get_active_material(0).set_shader_parameter("u_voxelDataTexture", finalImageTexture)
-	self.get_active_material(0).set_shader_parameter("u_directionalSunDirection", directionalLight.global_position)
+	self.get_active_material(0).set_shader_parameter("u_directionalSunDirection", -directionalLight.global_transform.basis.z)
+	self.get_active_material(0).set_shader_parameter("u_viewBoxDim", cubeDimensions)
 	
 	# optional code to save the image texture
 	if true:
 		ResourceSaver.save(finalImageTexture, "3d_texture.res")
 		
+
+
+func _process(delta: float) -> void:
+	# how to update chunks. Decently fast so could work with fairly large chunks
+	pass
+	
+	#if Input.is_action_just_pressed( "jump"):
+		#var images : Array[Image] = finalImageTexture.get_data()
+		#for x in range(0, cubeDimensions.x):
+			#for y in range(0, cubeDimensions.y):
+					#images[0].set_pixel(x, y, Color(1, 0, 0))
+		#finalImageTexture.update(images)
